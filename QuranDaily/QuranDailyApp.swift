a32fac79b93e9e2ad315830b4d461c997ddb9334
Ahsan Minhas <ahsan.minhas.official@gmail.com>
2026-05-30 00:23:38 +0500

    Initial Commit

---
+++ QuranDaily/QuranDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranDailyApp.swift
+//  QuranDaily
+//
+//  Created by Ahsan Minhas on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuranDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
